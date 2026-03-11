39f5ebf6feb1a8225356fd73dc68266a6ccd3eec
adikash21102002-cmyk <adikash21102002@gmail.com>
2026-03-11 11:26:34 +0530

    Refactor certificate verification and result display

--- script.js
+++ script.js
@@ -1,24 +1,96 @@
-function verifyCertificate() {
+let images=[
+"https://via.placeholder.com/800x250",
+"https://via.placeholder.com/800x250/ff0000",
+"https://via.placeholder.com/800x250/00ff00"
+]
 
-let cert = document.getElementById("cert").value;
+let i=0
 
-if(cert === "ACA123") {
+setInterval(function(){
 
-document.getElementById("result").innerHTML = "Certificate Valid";
+i++
+
+if(i>=images.length){
+i=0
+}
+
+document.getElementById("slide").src=images[i]
+
+},3000)
+
+function verify(){
+
+let n=document.getElementById("certno").value
+
+if(n=="ACA001"){
+
+document.getElementById("certresult").innerHTML="Certificate Valid"
 
 }
 
-else {
+else{
 
-document.getElementById("result").innerHTML = "Certificate Not Found";
+document.getElementById("certresult").innerHTML="Certificate Not Found"
+
+}
+
+}
+
+function result(){
+
+let r=document.getElementById("roll").value
+
+if(r=="101"){
+
+document.getElementById("resultshow").innerHTML="Result: PASS"
+
+}
+
+else{
+
+document.getElementById("resultshow").innerHTML="Result Not Found"
 
 }
 
 }
-
 
 function submitForm(){
 
-alert("Admission Form Submitted Successfully");
+alert("Admission Submitted")
+
+}
+
+function makeResume(){
+
+let name=document.getElementById("rname").value
+let q=document.getElementById("rqual").value
+let s=document.getElementById("rskill").value
+
+document.getElementById("resumeview").innerHTML=
+
+"<h3>"+name+"</h3><p>"+q+"</p><p>"+s+"</p>"
+
+}
+
+function makeID(){
+
+let name=document.getElementById("sname").value
+let course=document.getElementById("course").value
+let sid=document.getElementById("sid").value
+let photo=document.getElementById("photo").files[0]
+
+document.getElementById("showname").innerHTML="Name: "+name
+document.getElementById("showcourse").innerHTML="Course: "+course
+document.getElementById("showsid").innerHTML="ID: "+sid
+
+let reader=new FileReader()
+
+reader.onload=function(e){
+
+document.getElementById("showphoto").src=e.target.result
+
+}
+
+reader.readAsDataURL(photo)
 
 }
